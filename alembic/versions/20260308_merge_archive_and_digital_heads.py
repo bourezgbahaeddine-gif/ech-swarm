9835f9901a063b88f508209391c78ac6e20083d5
Bourezg Bahaeddine <bourezgbahaeddine@gmail.com>
2026-03-08 16:07:52 +0100

    Shorten archive merge revision id

--- alembic/versions/20260308_merge_archive_and_digital_heads.py
+++ alembic/versions/20260308_merge_archive_and_digital_heads.py
@@ -1,12 +1,12 @@
 """merge archive crawl and digital team heads
 
-Revision ID: 20260308_merge_archive_and_digital_heads
+Revision ID: 20260308_merge_archive_heads
 Revises: 20260308_archive_crawl_tables, 20260301_digital_team_module
 Create Date: 2026-03-08 15:10:00
 """
 
 
-revision = "20260308_merge_archive_and_digital_heads"
+revision = "20260308_merge_archive_heads"
 down_revision = ("20260308_archive_crawl_tables", "20260301_digital_team_module")
 branch_labels = None
 depends_on = None
